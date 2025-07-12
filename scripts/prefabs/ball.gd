ade3ef9b9b15c6c2e49708b6705725a75b4f4f4b
Endermandels <elijahdelavar@gmail.com>
2025-07-12 15:58:53 -0700

    Add ball collisions with walls and paddles

--- scripts/prefabs/ball.gd
+++ scripts/prefabs/ball.gd
@@ -1,12 +1,18 @@
-extends CharacterBody2D
+extends RigidBody2D
 class_name Ball
 
-@export_group("Internal Nodes")
-@export var movement_component: MovementComponent
+@export_group("Settings")
+@export var max_speed: float = 300
+@export var start_vector: Vector2 = Vector2(1, 1)
 
-var input_vector: Vector2 = Vector2(1, 1)
+func _ready() -> void:
+    linear_velocity = start_vector * max_speed
 
-func _physics_process(_delta: float) -> void:
-	movement_component.handle_movement(self, input_vector)
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+    # Make sure the ball is moving at a good horizontal speed
+    if absf(state.linear_velocity.x) < max_speed / 2:
+        state.linear_velocity.x = (max_speed / 2) * signf(state.linear_velocity.x)
 
-	move_and_slide()
+    # Make sure the ball isn't moving at super sonic speeds
+    if state.linear_velocity.length() > max_speed:
+        state.apply_central_impulse(-state.linear_velocity.normalized() * (state.linear_velocity.length() / 100))
